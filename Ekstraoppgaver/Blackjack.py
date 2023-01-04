002431a540660090074e94d887603aa302a9e1d1
e-haabre <hvdbreimo@gamil.com>
2023-01-04 09:59:36 +0100

    Utvidet spill funksjonen

--- Ekstraoppgaver/Blackjack.py
+++ Ekstraoppgaver/Blackjack.py
@@ -1,6 +1,17 @@
                         # Er ikke helt ferdig med denne koden, men den funker på det mest grunnleggende nivå. Skal kommentere den senere
-import random 
+import random
+import os
+command = 'cls'
 
+sort = ["hjerter", "ruter", "spa", "kløver"]
+kortstokk = {
+    "hjerter": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+    "ruter": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 
+    "spa": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 
+    "kløver": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+}
+
+""""
 hjerter = ["h_ace"]
 ruter = ["r_ace"]
 spa = ["s_ace"]
@@ -13,6 +24,10 @@ for i in range(2,14):
     kløver.append("k" + str(i))
 
 kortstokk = [hjerter, ruter, spa, kløver]
+"""
+def closest(lst, K):
+      
+    return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))]
 
 
 
@@ -26,10 +41,49 @@ class Blackjack:
     def motta(self):
         self.d_hand.append(self.d_take)
         
-    def holding(self):
-        print(f"Dealers hånd:\n{self.d_hand}")
+    def holding(self, vis):
+        if vis == True:
+            print(f"Dealers hånd:\n{self.d_hand}\nSum: {sum(self.d_hand)}")
+        
+        if sum(self.d_hand) > 21:
+            print("Dealer overtrådde 21")
+            return "Vinn"
+        return sum(self.d_hand)
 
-    #def spill(self):
+    def spill(self): 
+        os.system(command)
+        Kort(2, Player).valg()
+        Kort(1, Blackjack).valg()
+        Player(0).holding(True)
+        Blackjack(0).holding(True)
+
+        while Player(0).spill() == True:
+            os.system(command)
+            Kort(1,Player).valg()
+            Player(0).holding(True)
+
+        if Player(0).holding(False) == "Tap":
+            return
+
+        while Blackjack(0).holding(False) < 19:
+            os.system(command)
+            Kort(1,Blackjack).valg()
+        
+        if Blackjack(0).holding(False) == "Vinn":
+            return
+
+        resultat = []
+        resultat.append(Player(0).holding(False))
+        resultat.append(Blackjack(0).holding(False))
+        print(f"Du fikk {resultat[0]} og dealer fikk {resultat[1]}")
+
+        if resultat[0] == closest(resultat, 21):
+            print("Du vant")
+            
+
+        if resultat[1] == closest(resultat, 21):
+            print("Du tapte")
+
 
 
 class Kort:
@@ -41,10 +95,10 @@ class Kort:
     def stokk(self):
         self.valgt.clear()  
         s = random.randint(0, 3)
-        n = random.randint(0, len(kortstokk[s])-1)
+        n = random.randint(0, len(kortstokk[sort[s]])-1)
         #self.valgt.insert(0, kortstokk[s][n])
-        self.kort = kortstokk[s][n]
-        kortstokk[s].remove(self.kort)
+        self.kort = kortstokk[sort[s]][n]
+        kortstokk[sort[s]].remove(self.kort)
         #print(kortstokk)
         return self.kort
         
@@ -64,8 +118,14 @@ class Player:
     def motta(self):
         self.hand.append(self.take)
 
-    def holding(self):
-        print(f"Din hånd:\n{self.hand}")
+    def holding(self, vis):
+        if vis == True:
+            print(f"Din hånd:\n{self.hand}\nSum: {sum(self.hand)}")
+
+        if sum(self.hand) > 21:
+            print("Du overtrådde 21")
+            return "Tap"
+        return sum(self.hand)
 
     def spill(self):
         hit = input("Hit eller stand? \n>> ")
@@ -75,13 +135,4 @@ class Player:
         return self.hit
         
 
-
-Kort(2, Player).valg()
-Kort(1, Blackjack).valg()
-Player(0).holding()
-Blackjack(0).holding()
-
-while Player(0).spill() == True:
-    Kort(1,Player).valg()
-    Player(0).holding()
-
+Blackjack(0).spill()
